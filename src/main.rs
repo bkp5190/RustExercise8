d037d95445b1a2bd9abb6c94a6c7c62704738e98
Boney Patel <boneypatel37@yahoo.com>
2023-10-22 21:43:07 -0400

    Add child thread to join main thread

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,8 @@ fn main() {
     // to exit with a `Result<i32, Err>`.  Get the i32 out of the result and store it in a `sum`
     // variable.  Uncomment the println.  If you did 1a and 1b correctly, the sum should be 20.
     //
-    //let sum =
-    //println!("The child thread's expensive sum is {}", sum);
+    let sum = handle.join().unwrap();
+    println!("The child thread's expensive sum is {}", sum);
 
     // Time for some fun with threads and channels!  Though there is a primitive type of channel
     // in the std::sync::mpsc module, I recommend always using channels from the crossbeam crate,
